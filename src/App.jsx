47e4b8dcb55af64f6fa0aee586909a292f28c667
S Y <nikhil.sy@accenture.com>
2024-07-02 18:28:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import PasswordGenerator from './components/PasswordGenerator'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is Nikhil</h1>
+      <PasswordGenerator/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PasswordGenerator.jsx
@@ -0,0 +1,8 @@
+
+export default function PasswordGenerator() {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
